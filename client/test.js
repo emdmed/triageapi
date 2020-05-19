79f72c88800b5610dd11894f749eb179abd4ae47
nevor <nevikhun@gmail.com>
2020-05-19 00:50:18 -0300

    done

--- client/test.js
+++ client/test.js
@@ -1,13 +1,6 @@
 triageapi.render(".container", "patient")
+triageapi.renderSymptoms(patientModel, ".container")
+
 
 console.log(patientModel)
 
-
-$.ajax({
-    url: "https://triageapi.herokuapp.com/api/patientModel",
-    method: "GET",
-    headers: {"Authorization": "linkedin"},
-    success: function(res){
-        console.log(res);
-    }
-})
--- client/triagejs.js
+++ client/triagejs.js
@@ -99,44 +99,5 @@ const patientModel = {
     }
 }
 
-let triageapi = {
-    render,
-    patientModel
-}
 
-//append a card to an element
-function render(DOMelement, type){
 
-    console.log("run")
-
-    let elementToAppend;
-    if(type === "patient"){
-        elementToAppend = `
-        
-        <div class="card">
-        
-            <div class="card-body">
-
-                <h1>Patient</h1>
-
-            </div>
-
-        </div>
-        
-        `
-    }
-    console.log(DOMelement, type);
-    $(DOMelement).append(elementToAppend);
-}
-
-function renderSymptoms(patient, DOMelement){
-    symptoms = patient.symptoms;
-
-    for(key1 in symptoms){
-     
-        $(DOMelement).append(`
-            <button class="btn btn-outline-primary" id="${key1}">${symptoms[key]}</button>    
-            <hr>
-        `)
-    }
-}
