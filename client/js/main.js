9d628e7355446b9af4fe8edcfb59cf1fefb8ab6f
Enrique Darderes <enrique.darderes@gmail.com>
2020-11-11 12:15:16 -0300

    tryng to deasync getpatientModel() in triageapi.js

---
+++ client/js/main.js
@@ -0,0 +1,2 @@
+let patient = triageAPI.getPatientModel();
+console.log("Patient ", patient)
--- client/triagejs.js
+++ client/triagejs.js
@@ -1,106 +1,22 @@
-console.log("TRIAGEJS LOADED")
-const patientModel = {
-    info: {
-        gender: false,
-        age: false,
-        phone: false,
-        date: false,
-        number: null,
-        nomobile: false,
-        covidAlert: false
-    },
-    ruleOut: {
-        intensiveCareInLastMonth: false,
-        cancer: false,
-        immuneDefficiency: false,
-        chemoInLast2Months: false,
-        dyspnea: false,
-        acuteMotorImpairment: false,
-        chestPain: false,
-        hematemesis: false
-    },
-    symptoms: {
-        fever: {
-            isPresent: false,
-            measured: false,
-            durationInDays: null,
-            abortedWithAntipyretics: false
-        },
-        cough: {
-            isPresent: false,
-            sputum: {
-                isPresent: false,
-                color: {
-                    isGreen: false,
-                    isYellow: false,
-                    isTransparent: false,
-                    isWhite: false,
-                    isRed: false
-                },
+console.log("triage.js loaded...")
 
-            }
-        },
-        abdominalPain: {
-            isPresent: false,
-            durationInDays: null,
-            location: { //up to down right to left
-                one: false,
-                two: false,
-                three: false,
-                four: false,
-                five: false,
-                six: false,
-                seven: false,
-                eight: false, 
-                nine: false
-            }
-        },
-        throatPain: {
-            isPresent: false,
-            voiceChange: false,
-            durationInDays: null
-        },
-        runnyNose: {
-            isPresent: false, 
-            sputum: {
-                color: {
-                    isGreen: false,
-                    isYellow: false,
-                    isTransparent: false,
-                    isWhite: false,
-                    isRed: false
-                }
-            }
-        },
-        urinatingPain: {
-            isPresent: false,
-            durationInDays: null,
-            secretions: {
-                isPresent: false,
-                color: {
-                    isGreen: false,
-                    isYellow: false,
-                    isTransparent: false,
-                    isWhite: false,
-                    isRed: false
-                }
-            }
-        },
-        diarrhea: {
-            isPresent: false,
-            color: {
-                isRed: false,
-                isBlack: false
-            }
-        },
-        vomiting: {
-            isPresent: false
-        }
-    }
+const triageAPI = {
+    getPatientModel
 }
 
-
-
-
-
-
+function getPatientModel(){
+    let newdata = (async function(){
+        let data = await $.ajax({
+            url: "http://triageapi.herokuapp.com/api/patientModel",
+            method: "GET",
+            headers: {"Authorization": "linkedin"},
+            success: function(res){
+                let data = res;
+                console.log(data, "data")
+                return data;
+            }
+        })
+        return data
+    })();
+    return newdata
+}
