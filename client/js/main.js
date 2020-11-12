e1c7094b2fd82b743f1368a47ec484e5d9f96104
nevor <nevikhun@gmail.com>
2020-11-12 17:58:13 -0300

    deleted main.js

--- client/js/main.js
+++
@@ -1,9 +0,0 @@
-triageAPI.initPatient()
-triageAPI.setPatientAge(35)
-triageAPI.updatePatientSymptoms("fever", true)
-triageAPI.updatePatientSymptoms("cough", true)
-triageAPI.updatePatientSymptoms("sorongo", true)
-triageAPI.setRuleOut("intensiveCareInLastMonth", true)
-
-let scored = triageAPI.scorePatient();
-console.log(scored); // scored patient object returned
